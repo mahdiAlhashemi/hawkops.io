a3359e8d9997e9c541b1c1271d249b0e1c9be2f0
HatMast3r <hatmast3r@hotmail.com>
2025-12-03 23:54:56 +0300

    Update Dubizzle logo

--- src/data/clients.ts
+++ src/data/clients.ts
@@ -2,7 +2,7 @@ import type { Client } from '@/types'
 
 export const clients: Client[] = [
   { name: 'Creative', logo: '/images/clients/client1.png' },
-  { name: 'Dubizzle', logo: '/images/clients/dubizzle-logo.png' },
+  { name: 'Dubizzle', logo: '/images/clients/dubizzle-logo.webp' },
   { name: 'CuneiCode', logo: '/images/clients/CuneiCode.svg' },
   { name: 'Aldar', logo: '/images/clients/aldar.webp' },
   { name: 'Bayut', logo: '/images/clients/bayut.png' }
